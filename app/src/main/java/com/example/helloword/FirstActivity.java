87fb926b127ee6804c5f5ae5abd343ac120ebf96
Janus <janus.zou@outlook.com>
2018-11-13 08:57:52 +0800

    弹出信息  销毁活动  页面跳转

---
+++ app/src/main/java/com/example/helloword/FirstActivity.java
@@ -0,0 +1,65 @@
+package com.example.helloword;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //弹出信息
+                //Toast.makeText(FirstActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+                //销毁Activity 效果和按下返回键一样
+                //finish();
+                //显式Intent  活动之间跳转
+                //Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                //startActivity(intent);
+
+                //隐式Intent  活动之间跳转
+//                Intent intent = new Intent("com.example.helloword.ACTION_START");
+//                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+//                startActivity(intent);
+
+                //隐式Intent  打开其他网页
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://pan.baidu.com/s/11zmKrNWUb6NrTgLSpMrWrA"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    //显示菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    //调用菜单
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(FirstActivity.this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(FirstActivity.this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+                default:
+        }
+        return true;
+    }
+}
